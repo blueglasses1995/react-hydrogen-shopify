8abb819b14f7957eaa5e547e76e94a350445bf1e
blueglasses1995 <blueglasses1995@gmail.com>
2022-01-20 15:33:27 +0900

    add footer

--- src/components/Layout.server.jsx
+++ src/components/Layout.server.jsx
@@ -5,7 +5,7 @@ import gql from 'graphql-tag';
 // The `Layout` component accepts `children` as a prop. This injects any nested components into the layout when you call it.
 
 import Header from './Header.client';
-// import Footer from './Footer.server';
+import Footer from './Footer.server';
 
 export default function Layout({children}) {
   // Your shop domain and token are automatically configured, so you only need to pass in a GraphQL query.
@@ -16,18 +16,11 @@ export default function Layout({children}) {
   // Return JSX with the Tailwind classes that determine the layout styling.
   return (
     <div>
-      <div className="absolute top-0 left-0">
-        <a
-          href="#mainContent"
-          className="p-4 focus:block sr-only focus:not-sr-only"
-        >
-          Skip to content
-        </a>
-      </div>
       <Header storeName={data.shop.name} />
       <main id="mainContent" className="mx-auto max-w-7xl p-4 md:py-5 md:px-8">
         {children}
       </main>
+      <Footer />
     </div>
   );
 }
