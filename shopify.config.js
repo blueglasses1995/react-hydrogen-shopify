b4602c82d3bff455169be89dca830c62fa6d3b63
blueglasses1995 <blueglasses1995@gmail.com>
2022-01-20 12:13:21 +0900

    add main page and product page

---
+++ shopify.config.js
@@ -0,0 +1,6 @@
+export default {
+  locale: 'en-us',
+  storeDomain: 'grotesque-toy-stories.myshopify.com',
+  storefrontToken: 'a28473ea9a3364316a2ab6f5b3f54415',
+  graphqlApiVersion: 'unstable',
+};
