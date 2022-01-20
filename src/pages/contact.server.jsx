9b65426a9280107eb64ebcac925e152a486fcbaf
blueglasses1995 <blueglasses1995@gmail.com>
2022-01-20 15:48:57 +0900

    add about and contact page

---
+++ src/pages/contact.server.jsx
@@ -0,0 +1,22 @@
+export default function Index({first = 50}) {
+  return (
+    <>
+      <h2 className="text-4xl font-bold uppercase mb-4">Contact Us</h2>
+      <p className="text-xl font-thin mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente alias vero reiciendis ad? Veniam cum rerum ex. Modi natus labore impedit a deserunt ducimus dolores veritatis officiis? Delectus, cupiditate in!</p>
+      <p className="text-xl font-thin mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente alias vero reiciendis ad? Veniam cum rerum ex. Modi natus labore impedit a deserunt ducimus dolores veritatis officiis? Delectus, cupiditate in!</p>
+      <p className="text-xl font-thin mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente alias vero reiciendis ad? Veniam cum rerum ex. Modi natus labore impedit a deserunt ducimus dolores veritatis officiis? Delectus, cupiditate in!</p>
+      <p className="text-xl font-thin mb-16">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente alias vero reiciendis ad? Veniam cum rerum ex. Modi natus labore impedit a deserunt ducimus dolores veritatis officiis? Delectus, cupiditate in!</p>
+      <h2 className="text-4xl font-bold uppercase mb-4">Form</h2>
+      <p className="text-xl font-thin mb-4">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias pariatur, illum iste sit nemo at id dignissimos similique voluptates. Totam, aliquam! Est, in sequi cum exercitationem sit eum maxime consequatur?</p>
+      <p className="text-xl font-thin mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente alias vero reiciendis ad? Veniam cum rerum ex. Modi natus labore impedit a deserunt ducimus dolores veritatis officiis? Delectus, cupiditate in!</p>
+      <p className="text-xl font-thin mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente alias vero reiciendis ad? Veniam cum rerum ex. Modi natus labore impedit a deserunt ducimus dolores veritatis officiis? Delectus, cupiditate in!</p>
+      <p className="text-xl font-thin mb-16">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente alias vero reiciendis ad? Veniam cum rerum ex. Modi natus labore impedit a deserunt ducimus dolores veritatis officiis? Delectus, cupiditate in!</p>
+      <h2 className="text-4xl font-bold uppercase mb-4">Address</h2>
+      <p className="text-xl font-thin mb-4">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias pariatur, illum iste sit nemo at id dignissimos similique voluptates. Totam, aliquam! Est, in sequi cum exercitationem sit eum maxime consequatur?</p>
+      <h2 className="text-4xl font-bold uppercase mb-4">Phone</h2>
+      <p className="text-xl font-thin mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente alias vero reiciendis ad? Veniam cum rerum ex. Modi natus labore impedit a deserunt ducimus dolores veritatis officiis? Delectus, cupiditate in!</p>
+      <h2 className="text-4xl font-bold uppercase mb-4">SNS</h2>
+      <p className="text-xl font-thin mb-16">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente alias vero reiciendis ad? Veniam cum rerum ex. Modi natus labore impedit a deserunt ducimus dolores veritatis officiis? Delectus, cupiditate in!</p>
+    </>
+  );
+}
